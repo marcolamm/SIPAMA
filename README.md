900cbd7882110065c692c824af9744ce937e6300
simster <simster@hotmail.de>
2012-09-25 17:59:26 +0200

    Projekt erstellt

---
+++ README.md
@@ -0,0 +1,18 @@
+Projekt-GIT SIPAMA
+
+Collaborators:
+
+S S
+P D
+M L
+Installation
+You may fork this project using the github functionality .
+
+Usage:
+
+git clone git://github.com/marcolamm/SIPAMA.git
+cd SIPAMA
+mvn install
+
+
+Projekt MdP - ZHAW 17.09.2012 
---
+++ src/main/Start.java
@@ -0,0 +1,19 @@
+package main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
+public class Start {
+
+	private static final Logger logger = LoggerFactory.getLogger(Start.class);
+
+	public static void main(String[] args) {
+		logger.info("Start der App");
+		System.out.println("Hello World");
+		logger.info("Ende der App");
+
+	}
+
+}
